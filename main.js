583f7f96180f98dd06535e6d67a7aa1341b61eba
brandonking7 <kingrome74@gmail.com>
2017-03-08 13:29:23 -0500

    Everything plus a few functions

---
+++ main.js
@@ -0,0 +1,25 @@
+// window.onload = function() {
+
+//   // var response = prompt("Hi! What's your favorite food?");
+//   alert(`Welcome to Blackjack 21!`);
+
+// }
+  var cash = 100;
+  var bet = 100;
+  var cardValue = [];
+
+var button1 = document.querySelector('#bOne');
+  button1.addEventListener("click", function() {
+    alert('Thank you for clicking on me.')
+  });
+
+var button2 = document.querySelector('#bTwo');
+  button2.addEventListener("click", function() {
+    alert();
+  });
+
+var button3 = document.querySelector('#bThree');
+  button3.addEventListener("click", function() {
+    alert();
+  });
+
